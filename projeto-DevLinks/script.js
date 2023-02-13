d9bffe321c108edbd3a766043f19b8c5dab79122
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-02-13 09:19:32 -0300

    Update script.js

--- projeto-DevLinks/script.js
+++ projeto-DevLinks/script.js
@@ -2,10 +2,12 @@ function toggleMode() {
     const html = document.documentElement
 
     //condicional
-    if(html.classList.contains('light')) {
-      html.classList.remove('light')
-    } else {
-        html.classList.add('light')
-    }
+   // if(html.classList.contains('light')) {
+     // html.classList.remove('light')
+   // } else {
+    //    html.classList.add('light')
+   // }
     //no html na lista de classe contém o light
+
+    html.classList.toggle('light')//este toggle já existe no js
 }
