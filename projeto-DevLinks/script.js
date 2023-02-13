d693db621cc3cf4821bc2d7288ee22b86e47cc50
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2023-02-13 09:40:28 -0300

    Update script.js

--- projeto-DevLinks/script.js
+++ projeto-DevLinks/script.js
@@ -1,17 +1,16 @@
 function toggleMode() {
-    //as instruções estão na ordem de execussão 
-    const html = document.documentElement//pegando o html
-    html.classList.toggle('light')//fazendo a troca das classes
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-    //pegar tag img
-    const img = document.querySelector("#profile img")//seletor css para pegra a img usado p/ substituir img
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
-   //condicional - substituindo img
-   if(html.classList.contains('light')) {
-     //se tiver light mode, adicionar a img light
-     img.setAttribute('src', './assets/avatar-light.png')//modifica um atributo
-   } else {
-     //se estiver sem o light mode, manter a img normal
-     img.setAttribute('src', './assets/avatar-light.png')//modifica um atributo
-   }
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
